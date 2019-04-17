0c9f42c59a0afd50fdc56e603a8e72e85d7f431e
David Albela <david.albela@gnosis.pm>
2019-04-17 19:43:28 +0200

    Add $PROD_DEPLOYMENT_HOOK_ID project Id environment for Travis deployment

--- travis/prepare_production_deployment.sh
+++ travis/prepare_production_deployment.sh
@@ -11,7 +11,7 @@ then
      -F token="$PROD_DEPLOYMENT_HOOK_TOKEN" \
      -F ref=master \
      -F "variables[TRIGGER_RELEASE_COMMIT_TAG]=$TRAVIS_TAG" \
-      https://"$PROD_DEPLOYMENT_HOOK_URL_DOMAIN"/api/v4/projects/44/trigger/pipeline
+      https://"$PROD_DEPLOYMENT_HOOK_URL_DOMAIN"/api/v4/projects/$PROD_DEPLOYMENT_HOOK_ID/trigger/pipeline
 else
   echo "[ERROR] Production deployment could not be prepared"
 fi
