e037a1959174f772605880524a81fe76d451ceba
Angel Rodriguez <anxolin@gmail.com>
2019-04-26 12:05:31 +0200

    Allow to specify the tab in Etherscan

--- src/data/mock/safes.js
+++ src/data/mock/safes.js
@@ -6,12 +6,12 @@ module.exports = [
       { tokenA: 'WETH', tokenB: 'GNO' },
       { tokenA: 'OMG', tokenB: 'GNO' }
     ],
-    uniswapArbitrageAddress: '0x023456784567845678234567896786789565e7d6',
     operatorAddressIndex: 3, // 0x2dd2afa618f497efdb3a8c1707b06dc00b31fa19
     operatorAddress: '0x2dd2afa618f497efdb3a8c1707b06dc00b31fa19',
     safeAddress: '0xdb5dfe548e79441e709edd46f21d80cbd245b1be',
     safeModuleType: 'seller',
     safeModuleAddress: '0xa8fe9237c9b40d8b245d4df6050a51ecf5f878a0',
+    uniswapArbitrageAddress: '0xcd398d14bf8cc9695bf6a5fda75b468dd0ee7cb9',
     minimumAmountInUsdForToken: 3000
   },
 
@@ -23,7 +23,8 @@ module.exports = [
     operatorAddress: '0xdc102852b2bc57b860d4a47ab112374f2bf0c944',
     safeAddress: '0x9a6dd72f073e66ab5b1890e1643771669ab08ed1',
     safeModuleType: 'complete',
-    safeModuleAddress: '0xc483ef562ebca8f9b7b0bd8164d30d42e3876b24'
+    safeModuleAddress: '0xc483ef562ebca8f9b7b0bd8164d30d42e3876b24',
+    uniswapArbitrageAddress: '0x28707055b3c3f6b02eE5c3b02c805F230955B46E'
   },
 
   // RDN Safe
@@ -36,6 +37,7 @@ module.exports = [
     operatorAddress: '0xd954adf06a47e2577a382657e83bf8780435e7d6',
     safeAddress: '0x2f41a93b59acb94ca8cfd91178a2a537aed02bed',
     safeModuleType: 'complete',
-    safeModuleAddress: '0xc9c197a4b2b69cc73cc3ad52f498fc1192bf61c0'
+    safeModuleAddress: '0xc9c197a4b2b69cc73cc3ad52f498fc1192bf61c0',
+    uniswapArbitrageAddress: '0x9e24526EDb9dbc2872234935Ed92EF8c4bd79cc1'
   }
 ]
--- src/views/Safes/index.js
+++ src/views/Safes/index.js
@@ -10,125 +10,125 @@ import Web3HOC from '../../HOCs/Web3HOC'
 import getDxService from '../../services/dxService'
 
 const SAFE_TYPES = [
-    'seller',
-    'complete',   
+  'seller',
+  'complete',
 ]
 
 function Safes({ web3 }) {
-    const [safeData, setSafeData] = useState([])
-    const [safeNameFilter, setSafeNameFilter] = useState('')
-    const [safeTypeFilter, setSafeTypeFilter] = useState('')
-    const [network, setNetwork] = useState('UNKNOWN NETWORK')
+  const [safeData, setSafeData] = useState([])
+  const [safeNameFilter, setSafeNameFilter] = useState('')
+  const [safeTypeFilter, setSafeTypeFilter] = useState('')
+  const [network, setNetwork] = useState('UNKNOWN NETWORK')
 
-    // mount logic
-    // 1. load endpoint Safes data
-    // 2. set to state
-    useEffect(() => {
-        // load data
-        async function getSafeData(time = 3000) {
-            try {
-                const network = await web3.getNetworkId()
-                const dxService = await getDxService(network, web3)
+  // mount logic
+  // 1. load endpoint Safes data
+  // 2. set to state
+  useEffect(() => {
+    // load data
+    async function getSafeData(time = 3000) {
+      try {
+        const network = await web3.getNetworkId()
+        const dxService = await getDxService(network, web3)
 
-                const safeData = await dxService.getSafeModules()
-                
-                setNetwork(network)
-                return setSafeData(safeData)
-            } catch(error) {
-                const err = new Error(error.message)
-                console.error(err)
-            }
-        }
-        getSafeData()
-    }, [])
+        const safeData = await dxService.getSafeModules()
 
-    // eslint-disable-next-line eqeqeq
-    const renderEtherscanLink = address => <a href={`https://${network == '4' ? 'rinkeby.etherscan' : 'etherscan'}.io/address/${address}`} target="_blank" rel="noopener noreferrer">{address}</a>
-    
-    const renderAccountLink = address => address && <Link to={'/accounts/' + address}>{address}</Link>
+        setNetwork(network)
+        return setSafeData(safeData)
+      } catch (error) {
+        const err = new Error(error.message)
+        console.error(err)
+      }
+    }
+    getSafeData()
+  }, [])
 
-    return (
-        <PageWrapper pageTitle="DutchX Safes">
-            <Form>
-                <FormGroup row>
-                    {/* Filter SafeModule Name */}
-                    <Col sm={6} className="py-2">
-                        <PageFilter
-                            type="select"
-                            title="Safe"
-                            showWhat={safeNameFilter}
-                            changeFunction={event => setSafeNameFilter(event.target.value)}
-                            inputName="safe"
-                            render={safeData.map(({ name, safeAddress }) => <option key={safeAddress} value={name}>{name}</option>)}
-                        />
-                    </Col>
-                    {/* Filter SafeModule Type */}
-                    <Col sm={6} className="py-2">
-                        <PageFilter
-                            type="select"
-                            title="Type"
-                            showWhat={safeTypeFilter}
-                            changeFunction={event => setSafeTypeFilter(event.target.value)}
-                            inputName="safe"
-                            render={SAFE_TYPES.map((type, index) => <option key={index} value={type}>{type}</option>)}
-                        />
-                    </Col>
-                </FormGroup>
-            </Form>
+  // eslint-disable-next-line eqeqeq
+  const renderEtherscanLink = (address, section) => <a href={`https://${network == '4' ? 'rinkeby.etherscan' : 'etherscan'}.io/address/${address}${section ? '#' + section : ''}`} target="_blank" rel="noopener noreferrer">{address}</a>
 
-            <Table responsive hover>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        {/* <th>Markets</th>
+  const renderAccountLink = address => address && <Link to={'/accounts/' + address}>{address}</Link>
+
+  return (
+    <PageWrapper pageTitle="DutchX Safes">
+      <Form>
+        <FormGroup row>
+          {/* Filter SafeModule Name */}
+          <Col sm={6} className="py-2">
+            <PageFilter
+              type="select"
+              title="Safe"
+              showWhat={safeNameFilter}
+              changeFunction={event => setSafeNameFilter(event.target.value)}
+              inputName="safe"
+              render={safeData.map(({ name, safeAddress }) => <option key={safeAddress} value={name}>{name}</option>)}
+            />
+          </Col>
+          {/* Filter SafeModule Type */}
+          <Col sm={6} className="py-2">
+            <PageFilter
+              type="select"
+              title="Type"
+              showWhat={safeTypeFilter}
+              changeFunction={event => setSafeTypeFilter(event.target.value)}
+              inputName="safe"
+              render={SAFE_TYPES.map((type, index) => <option key={index} value={type}>{type}</option>)}
+            />
+          </Col>
+        </FormGroup>
+      </Form>
+
+      <Table responsive hover>
+        <thead>
+          <tr>
+            <th>Name</th>
+            {/* <th>Markets</th>
                         <th>Safe Address</th> */}
-                        <th>Type</th>
-                        <th>Info</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {safeData
-                        // Sort by operatorAddressIndex greatest to least
-                        .sort((a, b) => b.operatorAddressIndex - a.operatorAddressIndex)
-                        // filter out TYPE
-                        .filter(({ safeModuleType }) => safeTypeFilter ? safeModuleType === safeTypeFilter : true)
-                        // filter out NAME
-                        .filter(({ name }) => safeNameFilter ? name === safeNameFilter : true)
-                        .map(({
-                            name,
-                            markets,
-                            safeAddress,
-                            uniswapArbitrageAddress,
-                            // operatorAddressIndex,
-                            operatorAddress,
-                            safeModuleType,
-                            safeModuleAddress,
-                            // minimumAmountInUsdForToken,
-                        }) =>
-                        <tr key={`safe-${safeAddress}`}>
-                            {/* Safe Name */}
-                            <td><Badge color="primary" className="p-2" pill>{name}</Badge></td>
-                            {/* <td>{markets.map(({ tokenA, tokenB }) => [<p><Badge key={`safe-market-${tokenA}-${tokenB}`}>{`${tokenA}-${tokenB}`}</Badge></p>])}</td>
+            <th>Type</th>
+            <th>Info</th>
+          </tr>
+        </thead>
+        <tbody>
+          {safeData
+            // Sort by operatorAddressIndex greatest to least
+            .sort((a, b) => b.operatorAddressIndex - a.operatorAddressIndex)
+            // filter out TYPE
+            .filter(({ safeModuleType }) => safeTypeFilter ? safeModuleType === safeTypeFilter : true)
+            // filter out NAME
+            .filter(({ name }) => safeNameFilter ? name === safeNameFilter : true)
+            .map(({
+              name,
+              markets,
+              safeAddress,
+              uniswapArbitrageAddress,
+              // operatorAddressIndex,
+              operatorAddress,
+              safeModuleType,
+              safeModuleAddress,
+              // minimumAmountInUsdForToken,
+            }) =>
+              <tr key={`safe-${safeAddress}`}>
+                {/* Safe Name */}
+                <td><Badge color="primary" className="p-2" pill>{name}</Badge></td>
+                {/* <td>{markets.map(({ tokenA, tokenB }) => [<p><Badge key={`safe-market-${tokenA}-${tokenB}`}>{`${tokenA}-${tokenB}`}</Badge></p>])}</td>
                             <td>{renderAccountLink(safeAddress)}</td> */}
-                            {/* Type */}
-                                <td><Badge color="success" className="p-2" pill>{safeModuleType}</Badge></td>
-                            {/* Safe Info */}
-                            <td>
-                                <ul>
-                                    <li>Markets: {markets.map(({ tokenA, tokenB }) => [<span key={`safe-market-${tokenA}-${tokenB}`} style={{padding: '0px 5px'}}><Badge>{`${tokenA}-${tokenB}`}</Badge></span>])}</li>
-                                    <li>Safe: {renderAccountLink(safeAddress)}</li>
-                                    <li>Safe Module: {renderEtherscanLink(safeModuleAddress)}</li>
-                                    {uniswapArbitrageAddress && <li>Uniswap: {renderAccountLink(uniswapArbitrageAddress)}</li>}
-                                    {/* Operator Address */}
-                                    <li>Operator: {renderEtherscanLink(operatorAddress)}</li>
-                                </ul>
-                            </td>
-                        </tr>
-                    )}
-                </tbody>
-            </Table>
-        </PageWrapper>
-    )
+                {/* Type */}
+                <td><Badge color="success" className="p-2" pill>{safeModuleType}</Badge></td>
+                {/* Safe Info */}
+                <td>
+                  <ul>
+                    <li>Markets: {markets.map(({ tokenA, tokenB }) => [<span key={`safe-market-${tokenA}-${tokenB}`} style={{ padding: '0px 5px' }}><Badge>{`${tokenA}-${tokenB}`}</Badge></span>])}</li>
+                    <li>Safe: {renderAccountLink(safeAddress)}</li>
+                    <li>Safe Module: {renderEtherscanLink(safeModuleAddress)}</li>
+                    {uniswapArbitrageAddress && <li>Uniswap's arbitrage contract: {renderEtherscanLink(uniswapArbitrageAddress, 'readContract')}</li>}
+                    {/* Operator Address */}
+                    <li>Operator: {renderEtherscanLink(operatorAddress)}</li>
+                  </ul>
+                </td>
+              </tr>
+            )}
+        </tbody>
+      </Table>
+    </PageWrapper>
+  )
 }
 
 export default ErrorHOC(Web3HOC(Safes))
