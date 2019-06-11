dad84575afc57140d29b8f2510cfef84abeef6db
Anxo Rodriguez <anxolin@gmail.com>
2019-06-11 09:35:48 +0200

    Add MGN holders page

---
+++ src/views/MgnHolders/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function MgnHolders() {
+  return (
+    <iframe
+      title="MGN Holders"
+      src="https://explore.duneanalytics.com/embed/query/294/visualization/452?api_key=EfkCCXEO4zXRUB8uDwf8RbCsf4L9vVDT0XMzYaZ4&"
+      width="100%"
+      height="1100px">
+    </iframe>
+  )
+}
+
+export default MgnHolders
