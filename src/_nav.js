07a146ebe8e1c58462ed99c6abbcff8829397835
David <david.sato64@gmail.com>
2019-06-12 10:57:34 +0200

    add env variable REACT_APP_PUBLIC
    1. signifies build of public, VisualX app
    2. Removes Bots/Info/Safes
    3. Changes app title in certain places

--- src/_nav.js
+++ src/_nav.js
@@ -1,63 +1,65 @@
+const isPublicApp = process.env.REACT_APP_PUBLIC
+
+const navArray = [
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    icon: 'icon-speedometer',
+  },
+  {
+    title: true,
+    name: isPublicApp ? 'VisualX' : 'Admin',
+    wrapper: {
+      element: '',
+      attributes: {}
+    },
+    class: ''
+  },
+  {
+    name: 'Markets',
+    url: '/markets',
+    icon: 'fa fa-line-chart',
+  },
+  {
+    name: 'MGN Holders',
+    url: '/mgn-holders',
+    icon: 'cui-briefcase icons',
+  },
+  {
+    name: 'Check balances',
+    url: '/accounts',
+    icon: 'fa fa-money',
+  },
+]
+
+// Regular admin page
+if (!isPublicApp) navArray.push(
+  {
+    name: 'Bots list',
+    url: '/bots/list',
+    icon: 'fa fa-heartbeat',
+  },
+  {
+    name: 'Safes',
+    url: '/safes',
+    icon: 'fa fa-lock',
+  },
+  {
+    title: true,
+    name: 'About',
+    wrapper: {
+      element: '',
+      attributes: {}
+    },
+    class: '',
+  },
+  {
+    name: 'Bots',
+    url: '/bots',
+    icon: 'fa fa-info',
+  }
+)
+
 export default {
-  items: [
-    {
-      name: 'Dashboard',
-      url: '/dashboard',
-      icon: 'icon-speedometer'
-      /*
-      badge: {
-        variant: 'info',
-        text: 'NEW',
-      },
-      */
-    },
-    {
-      title: true,
-      name: 'Admin',
-      wrapper: {            // optional wrapper object
-        element: '',        // required valid HTML5 element tag
-        attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
-      },
-      class: ''             // optional class names space delimited list for title item ex: "text-center"
-    },
-    {
-      name: 'Markets',
-      url: '/markets',
-      icon: 'fa fa-line-chart',
-    },
-    {
-      name: 'MGN Holders',
-      url: '/mgn-holders',
-      icon: 'cui-briefcase icons',
-    },
-    {
-      name: 'Bots list',
-      url: '/bots/list',
-      icon: 'fa fa-heartbeat',
-    },
-    {
-      name: 'Safes',
-      url: '/safes',
-      icon: 'fa fa-lock',
-    },
-    {
-      name: 'Check balances',
-      url: '/accounts',
-      icon: 'fa fa-money',
-    },
-    {
-      title: true,
-      name: 'About',
-      wrapper: {            // optional wrapper object
-        element: '',        // required valid HTML5 element tag
-        attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
-      },
-      class: ''             // optional class names space delimited list for title item ex: "text-center"
-    },
-    {
-      name: 'Bots',
-      url: '/bots',
-      icon: 'fa fa-info',
-    }
-  ],
+  items: navArray,
 };
--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { Jumbotron, Container } from 'reactstrap';
+import React from 'react'
+import { Jumbotron, Container } from 'reactstrap'
 
 import ErrorHOC from '../../HOCs/ErrorHOC'
 
 import { DX_GRAPHS_URL } from '../../globals'
+
+const isPublic = process.env.REACT_APP_PUBLIC
 
 function Dashboard() {
   return (
     <div className="animated fadeIn">
       <Jumbotron fluid>
         <Container fluid>
-          <h1 className="display-3">DutchX services admin</h1>
+          <h1 className="display-3">{isPublic ? 'VisualX' : 'DutchX Services Admin'}</h1>
           <p className="lead">
-            Admin bots, services, and notifications.
+            DutchX - Visualised
           </p>
           <iframe id="dxDashboardGraphs" title="DutchX Graphs" src={DX_GRAPHS_URL} style={{ height: '100vh', width: '100%' }}/>
         </Container>
@@ -21,4 +23,4 @@ function Dashboard() {
   )
 }
 
-export default ErrorHOC(Dashboard);
+export default ErrorHOC(Dashboard)
