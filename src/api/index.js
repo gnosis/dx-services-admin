a722bd3995de85b9237ba987e8552bd27a1627d7
David <david.sato64@gmail.com>
2019-07-01 15:40:27 +0200

    created API methods

---
+++ src/api/index.js
@@ -0,0 +1,19 @@
+import getDxService from '../services/dxService'
+
+// Grab DX instance
+export const getDxContract = async (net, web3) => web3.getDutchX(net)
+// Grab Tokens and Network
+export async function getTokensAndNetwork(web3Instance, networkParam) {
+    try {
+      const bcNetwork = networkParam || await web3Instance.getNetworkId()
+      const dxService = await getDxService(bcNetwork, web3Instance)
+      
+      // get all available tokens on DutchX Protocol
+      const tokens = await dxService.getTokens()
+
+      return { tokens, bcNetwork }
+    } catch (error) {
+      console.error(error)
+      throw new Error(error)
+    }
+}
